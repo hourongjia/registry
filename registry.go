984dc36b55d791411a3d619a2c26f1a777db45e6
侯荣嘉 <hourongjia@didachuxing.com>
2023-07-04 11:55:26 +0800

    tijiao

--- registry.go
+++ registry.go
@@ -27,32 +27,66 @@ func NewRegistryCache(bucketCnt uint16) (RegistryCache, error) {
 }
 
 type CommonRegistry struct {
-	Cache *RegistryCache
+	Subject Subject
+	Cache   *RegistryCache
 }
 
 func (receiver *CommonRegistry) AddData(key string, value string) error {
 	receiver.Cache.Cache.Put(key, value)
 	// 注册到注册中新
+	event := Event{
+		Async:     false,
+		EventType: Add,
+		Data:      EventData{},
+	}
+	receiver.Subject.NotifyObservers(event)
 	return nil
 }
 
 func (receiver *CommonRegistry) DeleteData(key string) error {
 	// 从注册中心删除
 	receiver.Cache.Cache.Del(key)
+	// 注册到注册中新
+	event := Event{
+		Async:     false,
+		EventType: Delete,
+		Data:      EventData{},
+	}
+	receiver.Subject.NotifyObservers(event)
 	return nil
 }
 
 func (receiver *CommonRegistry) GetData(key string) (interface{}, error) {
-	// 从注册中心获取数据
+	// 注册到注册中新
+	event := Event{
+		Async:     false,
+		EventType: Select,
+		Data:      EventData{},
+	}
+	receiver.Subject.NotifyObservers(event)
 	return receiver.Cache.Cache.Get(key), nil
 }
 
 func (receiver *CommonRegistry) AddDataWithDeadLine(key string, value string, deadline float64) error {
+	// 注册到注册中新
+	event := Event{
+		Async:     false,
+		EventType: Add,
+		Data:      EventData{},
+	}
+	receiver.Subject.NotifyObservers(event)
 	// 设置deadline到注册中心
 	return nil
 }
 
 func (receiver *CommonRegistry) UpdateDataTime(key string, value float64) error {
+	// 注册到注册中新
+	event := Event{
+		Async:     false,
+		EventType: Update,
+		Data:      EventData{},
+	}
+	receiver.Subject.NotifyObservers(event)
 	// 更新数据完成时间戳
 	return nil
 }
